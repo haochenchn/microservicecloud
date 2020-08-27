5605fa90ba0cf76df55aefbe87741939a2ab1f7c
haochenchn <haochenchn@163.com>
2020-08-27 23:54:03 +0800

    重拾微服务

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -2,29 +2,39 @@ package com.atguigu.springcloud.entities;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 
-
-@NoArgsConstructor
-//@AllArgsConstructor
-@Data
-@Accessors(chain=true)
 public class Dept implements Serializable// entity --orm--- db_table
 {
-	private Long 	deptno; // 主键
-	private String 	dname; // 部门名称
-	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	private Long 	dNo; // 主键
+	private String 	dName; // 部门名称
+	private String 	dbSource;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	
-	public Dept(String dname)
+	public Dept(Long dNo)
 	{
-		super();
-		this.dname = dname;
+		this.dNo = dNo;
 	}
-	
-	
+
+	public Long getdNo() {
+		return dNo;
+	}
+
+	public void setdNo(Long dNo) {
+		this.dNo = dNo;
+	}
+
+	public String getdName() {
+		return dName;
+	}
+
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+
+	public String getDbSource() {
+		return dbSource;
+	}
+
+	public void setDbSource(String dbSource) {
+		this.dbSource = dbSource;
+	}
 }
